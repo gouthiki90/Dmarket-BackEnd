4ca242f94811f957425b8a754c089f5873e79813
전가은 <jeonga-eun@jeonga-eun-ui-MacBookAir.local>
2022-05-23 21:46:11 +0900

    Inital commit

---
+++ src/test/java/site/dmarket/dmarketbackend/DmarketBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package site.dmarket.dmarketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmarketBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
